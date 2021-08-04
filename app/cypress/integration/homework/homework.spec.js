76b0b62ae489b5e3252cde4ea6cb755ab4ddf809
ali.li@titansoft.com.sg <>
2021-08-05 00:21:16 +0800

    add integration tests for homework

---
+++ app/cypress/integration/homework/homework.spec.js
@@ -0,0 +1,29 @@
+describe("group page operate", function () {
+    beforeEach(() => {
+        cy.visit('localhost:3000');
+    })
+    it("home page will see element correctly", function() {
+        cy.findByText("Home").should("exist");
+        cy.findByText("Groups").should("exist");
+        cy.findByText("End-to-End Testing with Cypress.io Workshop").should("exist");
+        cy.findByText("Visit cypress.io to read the documentation").should("exist");
+    })
+    it("the address in group page can be edit", function() {
+        cy.findByText("Groups").should("exist").click();
+        cy.url().should("include", "/groups");
+        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("exist").then((element) => {
+            cy.get(element).parent().should("exist").findByText("Edit").should("exist").click();
+        });
+        cy.findByLabelText("Address").click().type("AAA");
+        cy.findByText("Submit").should("exist").click();
+        cy.findByText("AAA").should("exist");
+    })
+    it("the data in group page can be delete", function() {
+        cy.findByText("Groups").should("exist").click();
+        cy.url().should("include", "/groups");
+        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("exist").then((element) => {
+            cy.get(element).parent().should("exist").findByText("Delete").should("exist").click();
+        });
+        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("not.exist");
+    })
+})
