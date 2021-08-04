b368451e915d076063e9f0a32fd42b44dcd3ae29
ali.li@titansoft.com.sg <>
2021-08-05 00:50:35 +0800

    make test case independently

--- app/cypress/integration/homework/homework.spec.js
+++ app/cypress/integration/homework/homework.spec.js
@@ -9,9 +9,26 @@ describe("group page operate", function () {
         cy.findByText("Visit cypress.io to read the documentation").should("exist");
     })
     it("the address in group page can be edit", function() {
+        cy.request('POST', 'http://localhost:8080/api/group', {
+            "name": "Shenzhen Tech Community",
+            "address": null,
+            "city": null,
+            "stateOrProvince": null,
+            "country": null,
+            "postalCode": null,
+            "user": null,
+            "events": [
+                {
+                    "date": "2021-09-15T17:30:00Z",
+                    "title": "Visual Testing with yeongsheng",
+                    "description": "Visual Testing with Cypress + Percy",
+                    "attendees": []
+                }
+            ]
+        });
         cy.findByText("Groups").should("exist").click();
         cy.url().should("include", "/groups");
-        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("exist").then((element) => {
+        cy.findByText("Sep 16, 2021 : Visual Testing with yeongsheng").should("exist").then((element) => {
             cy.get(element).parent().should("exist").findByText("Edit").should("exist").click();
         });
         cy.findByLabelText("Address").click().type("AAA");
@@ -21,9 +38,9 @@ describe("group page operate", function () {
     it("the data in group page can be delete", function() {
         cy.findByText("Groups").should("exist").click();
         cy.url().should("include", "/groups");
-        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("exist").then((element) => {
+        cy.findByText("Sep 16, 2021 : Visual Testing with yeongsheng").should("exist").then((element) => {
             cy.get(element).parent().should("exist").findByText("Delete").should("exist").click();
         });
-        cy.findByText("Sep 16, 2021 : Visual Testing with Percy").should("not.exist");
+        cy.findByText("Sep 16, 2021 : Visual Testing with yeongsheng").should("not.exist");
     })
 })
